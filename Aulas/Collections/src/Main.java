4318b6880b18004490da0cc377115bec9305e5f4
KaueMagnus <kmarquesmagnus@gmail.com>
2025-06-23 20:48:40 -0300

    add: Aula sobre Collections

---
+++ Aulas/Collections/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> frutas = new LinkedList<>();
+        frutas.add("Laranja");
+        frutas.add("Abacate");
+        System.out.println(frutas);
+
+        frutas.
+        System.out.println(frutas.contains("Laranja"));
+    }
+}
